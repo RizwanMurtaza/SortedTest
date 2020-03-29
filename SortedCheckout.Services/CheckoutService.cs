af88aba87797f5e059c51b28222d9879cf5dd4bd
rizwanmurtaza <rizwan.murtaza.dev@gmail.com>
2020-03-29 18:20:27 +0100

    Added hard coded dictionary to get the speial item prices and calculating the total discount  .. Test status -- GREEN

--- SortedCheckout.Services/CheckoutService.cs
+++ SortedCheckout.Services/CheckoutService.cs
@@ -18,9 +18,27 @@ namespace SortedCheckout.Services
             if (!_scannedItems.Any())
                 return 0;
 
-            return _scannedItems.Sum(x => x.Price);
+            var totalDiscount = 0.0;
+            foreach (var item in GetSpecialPriceItems())
+            {
+                var currentItem = _scannedItems.Where(x => x.SKU.Equals(item.Key));
+                var discountQuantity = currentItem.Count() / item.Value.Quantity;
+                totalDiscount += discountQuantity * item.Value.Discount;
+            }
+            return _scannedItems.Sum(x => x.Price) - totalDiscount;
+        }
+        private Dictionary<string, DiscountQuantityPair> GetSpecialPriceItems()
+        {
+            return new Dictionary<string, DiscountQuantityPair>
+            {{"A99", new DiscountQuantityPair(){Discount = 0.20, Quantity = 3}},
+                { "B15", new DiscountQuantityPair() { Discount = 0.15, Quantity = 2 } } };
         }
 
-
+    }
+    struct DiscountQuantityPair
+    {
+        public int Quantity;
+        public double Discount;
     }
 }
+

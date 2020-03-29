89d987a7e15d523395d6141c530b79b80046b914
rizwanmurtaza <rizwan.murtaza.dev@gmail.com>
2020-03-29 18:06:32 +0100

    Refactor added new domain project and new model of scanned items

---
+++ SortedCheckout.Domain/CheckoutItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SortedCheckout.Domain
+{
+    public class CheckoutItem
+    {
+        public string SKU { get; set; }
+        public double Price { get; set; }
+        public CheckoutItem(string sku, double price)
+        {
+            SKU = sku;
+            Price = price;
+        }
+    }
+}

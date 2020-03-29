40fc2dcfc69096d4b5d4ca0cddcbd44572aa4d0c
rizwanmurtaza <rizwan.murtaza.dev@gmail.com>
2020-03-29 17:37:51 +0100

    Intial solution structure commit

---
+++ SortedCheckout/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SortedCheckout
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

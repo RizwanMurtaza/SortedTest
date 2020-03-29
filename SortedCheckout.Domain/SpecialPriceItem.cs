42bd94c96eb350f5ace72a6c02cad3f79f01dbe1
rizwanmurtaza <rizwan.murtaza.dev@gmail.com>
2020-03-29 18:22:46 +0100

    Added new class SpecialPriceItem

---
+++ SortedCheckout.Domain/SpecialPriceItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortedCheckout.Domain
+{
+    public class SpecialPriceItem
+    {
+        //public CheckoutItem Item { get; set; }
+        public string SKU { get; set; }
+        public int QuantityToBuyForQualify { get; set; }
+        public double TotalDiscount { get; set; }
+    }
+}
